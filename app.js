2ef86580c27e4e1dd708add3a446f05c67ac21aa
Pascal <pascalokereke18@gmail.com>
2024-01-11 14:22:20 +0100

    the js keyup function

--- app.js
+++ app.js
@@ -1,5 +1,43 @@
 // javascript
-// form action on the search icon
+// join online button
+// make the join online accessible for keyboard users.
+
+function joinOnline() {
+    document.querySelector('#join-online').addEventListener('keyup', (e) => {
+        if(e.key === 'Enter'){
+             e.target.click();
+        }
+    })
+}
+
+// make the Listen now button accessible for keyboard users
+
+function listenNow() {
+    document.querySelector('#listen-now').addEventListener('keyup', (e) => {
+        if(e.key === 'Enter'){
+            e.target.click();
+        }
+    })
+}
 
 
+// make the view schools button accessible for keyboard users.
+
+function viewSchools() {
+    document.querySelector('#view-schools').addEventListener('keyup', (e) => {
+        if(e.key === 'Enter'){
+            e.target.click();
+        }
+    })
+}
+
+// make the subscribe button accessible for keyboard users
+
+function subscribeButton() {
+    document.querySelector('#mc-embedded-subscribe').addEventListener('keyup', (e) => {
+        if(e.key === 'Enter'){
+            e.target.click();
+        }
+    })
+}
 
