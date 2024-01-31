89959a7406d4c43b024806f3bd9aef38e44e7ea5
Pascal <pascalokereke18@gmail.com>
2024-01-31 21:45:35 +0100

    still didnt work

--- app.js
+++ app.js
@@ -86,3 +86,21 @@ function toggleMenu(){
     menuIcon.classList.toggle('open');
 }
 
+// menu-icon to dropdown the menu.
+document.getElementById('menuIcon').addEventListener('click', ()=> {
+    let dropdown = document.getElementById('w-dropdown-toggle-0');
+    dropdown.style.display = dropdown.style.display === "block" ? "none" : "block"; 
+});
+
+// close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if(!event.target.matches('#menuIcon')) {
+        let dropdowns = document.getElementsByClassName("dropdown-toggle about w-dropdown-toggle");
+        for (let i = 0; i < dropdowns.length; i++){
+            let openDropdown = dropdowns[i];
+            if(openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
